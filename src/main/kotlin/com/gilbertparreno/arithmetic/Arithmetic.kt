91ae8c4a12f05a18eb5b38cb9133716822f76c8d
Gilbert Parreno <gilbertparreno95@gmail.com>
2021-04-08 15:43:55 +0800

    Initial commit

---
+++ src/main/kotlin/com/gilbertparreno/arithmetic/Arithmetic.kt
@@ -0,0 +1,8 @@
+package com.gilbertparreno.arithmetic
+
+object Arithmetic {
+    fun add(a: Float, b: Float) = a + b
+    fun subtract(a: Float, b: Float) = a - b
+    fun multiply(a: Float, b: Float) = a * b
+    fun divide(a: Float, b: Float) = a / b
+}
