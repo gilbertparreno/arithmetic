1990e6a6d78b296571562cc34d9e80a25a321ad2
Gilbert Parreno <gilbertparreno95@gmail.com>
2021-04-08 17:00:07 +0800

    build.gradle fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,39 @@
-plugins {
-    kotlin("jvm") version "1.4.10"
+import extensions.implementation
+import org.gradle.api.JavaVersion
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+group = "arithmetic"
+version = "1.0"
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath(ApplicationDependencies.GradlePlugins.kotlinGradlePlugin)
+    }
 }
 
-group = "com.gilbertparreno"
-version = "1.0-SNAPSHOT"
+plugins {
+    id("java")
+    id("maven")
+    kotlin("jvm") version ApplicationDependencyVersions.kotlin
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
 
 repositories {
     mavenCentral()
+    maven("https://dl.bintray.com/babylonpartners/maven")
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
+    implementation(ApplicationDependencies.dependencies)
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
 }
