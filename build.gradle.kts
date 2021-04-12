d64ed2bc53ac415cd6acf747fc08ba6a174aa9af
Gilbert Parreno <gilbertparreno95@gmail.com>
2021-04-12 15:06:52 +0800

    set up maven publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
 import extensions.implementation
-import org.gradle.api.JavaVersion
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-group = "io.github.gilbertparreno"
-version = "1.0"
+group = ApplicationConfiguration.groupId
+version = ApplicationConfiguration.version
 
 buildscript {
     repositories {
@@ -15,8 +14,9 @@ buildscript {
 }
 
 plugins {
-    id("java")
-    id("maven")
+    id("java-library")
+    id("maven-publish")
+    id("signing")
     kotlin("jvm") version ApplicationDependencyVersions.kotlin
 }
 
@@ -36,4 +36,74 @@ dependencies {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = ApplicationConfiguration.artifactId
+            from(components["java"])
+            artifact(sourcesJar.get())
+            artifact(javadocJar.get())
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set(ApplicationConfiguration.name)
+                description.set(ApplicationConfiguration.description)
+                url.set(ApplicationConfiguration.url)
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/gilbertparreno/arithmetic.git")
+                    developerConnection.set("scm:git:git@github.com:gilbertparreno/arithmetic.git")
+                    url.set("https://github.com/gilbertparreno/arithmetic")
+                }
+                developers {
+                    developer {
+                        id.set("gilbertparreno")
+                        name.set("Gilbert Parreño")
+                        email.set("gilbertparreno95@gmail.com")
+                    }
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            val repositoryUrl = if (version.toString().endsWith("SNAPSHOT")) {
+                ApplicationConfiguration.snapshotRepositoryUrl
+            } else {
+                ApplicationConfiguration.releaseRepositoryUrl
+            }
+            url = uri(repositoryUrl)
+            credentials {
+                username = rootProject.findProperty("ossrhUsername") as? String
+                password = rootProject.findProperty("ossrhPassword") as? String
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }
--- buildSrc/src/main/kotlin/ApplicationConfiguration.kt
+++ buildSrc/src/main/kotlin/ApplicationConfiguration.kt
@@ -1,1 +1,10 @@
-object ApplicationConfiguration
+object ApplicationConfiguration {
+    const val artifactId = "arithmetic"
+    const val groupId = "io.github.gilbertparreno"
+    const val version = "1.1.2"
+    const val name = "Arithmetic"
+    const val url = "https://gilbertparreno.github.io/"
+    const val description = "This is a test repository by Gilbert Parreño"
+    const val releaseRepositoryUrl = "https://s01.oss.sonatype.org/content/repositories/releases/"
+    const val snapshotRepositoryUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+}
